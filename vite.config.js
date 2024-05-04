ad2ca499753f7d74ca4d12fa8753b381b1ef9f47
yi-chilin <jeffery017@gmail.com>
2024-05-03 21:24:15 -0500

    setting deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/pinterest-website-clone"
+  base: "/pinterest-website-clone/"
 })
